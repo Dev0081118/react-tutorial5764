4d0a2375ed49f97a003cee6e7e12b9f0263ff28a
Dashboard <divyarajsinhrana@gmail.com>
2026-07-25 00:19:16 +0530

    first commit

---
+++ babatilu/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="parent">
+      <h1 id="child1">Hello, World!</h1>
+      <h2 id="child2">Welcome to my React App</h2>
+    </div>
+    <div id="parent2">
+      <h1 id="child3">This is another parent div</h1>
+      <h2 id="child4">With its own children</h2>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
